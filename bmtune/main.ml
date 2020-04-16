d7cadd53a41b4f28423936045d0734f56a79cde8
Magnus Skjegstad <magnus@skjegstad.com>
2020-04-17 00:18:34 +0200

    Add irq reconfig, cpu governor, turbo boost disable

--- bmtune/main.ml
+++ bmtune/main.ml
@@ -23,11 +23,17 @@ let parse_core_list s =
         Cores.of_list (List.concat (List.map process_elt split))
 
 (* read full content of file and return as string *)
-let read_file f =
-        let c = open_in f in
+let read_file path =
+        let c = open_in path in
         let line = input_line c in
         close_in c;
         line
+
+(* write file content *)
+let write_file path str =
+        let c = open_out path in
+        fprintf c "%s\n" str;
+        close_out c
 
 (* check if smt/ht is enabled *)
 let smt_enabled () =
@@ -36,9 +42,11 @@ let smt_enabled () =
         | _ -> false
 
 let smt_disable () =
-        let f = open_out "/sys/devices/system/cpu/smt/control" in
-        fprintf f "%s\n" "off";
-        close_out f
+        write_file "/sys/devices/system/cpu/smt/control" "off"
+
+let string_of_cores c =
+        let m = Cores.fold (fun x a -> string_of_int x::a) c [] in
+        String.concat "," (List.rev m)
 
 let () =
         printf "-- checking and preparing system for benchmarking ---\n";
@@ -79,12 +87,50 @@ let () =
         end;
 
         (* check irqs *)
-        printf "IRQ interrupts: \n";
-        
+        printf "IRQ interrupt assignment: \n";
+        let irq_path = "/proc/irq" in
+        (* some IRQs can't be reassigned *)
+        let ignore_irqs = [0;2;130] in
+        Array.iter (fun irq ->
+                (* /proc/irq/*/smp_affinity_list *)
+                let f = Filename.concat (Filename.concat irq_path irq) "smp_affinity_list" in
+                if (Sys.file_exists f) then begin
+                        let irq_cores = parse_core_list (read_file f) in
+                        printf " - IRQ %s, " irq;
+                        if not (List.mem (int_of_string irq) ignore_irqs) then begin
+                                if not (Cores.disjoint isol_cores irq_cores) then begin 
+                                        let remaining_cores = Cores.diff irq_cores isol_cores in
+                                        printf "reconfiguring (%s) -> (%s)..." (string_of_cores irq_cores) (string_of_cores remaining_cores);
+                                        write_file f (string_of_cores remaining_cores);
+                                        printf "\n"
+                                end else
+                                        printf "ok\n"
+                        end else
+                                printf "ignored\n"
+                end
+        ) (Sys.readdir irq_path);
+                
+        (* check intel turbo boost  *)
+        printf "Disabling turbo boost: ";
+        let no_turbo_f = "/sys/devices/system/cpu/intel_pstate/no_turbo" in
+        if (Sys.file_exists no_turbo_f) then begin
+                write_file no_turbo_f "1";
+                printf "intel_pstabe/no_turbo=1 "
+        end;
+        let boost_f = "/sys/devices/system/cpu/cpufreq/boost" in
+        if (Sys.file_exists boost_f) then begin
+                write_file boost_f "0";
+                printf "cpufreq/boost=0 "
+        end;
+        printf "\n";
 
-        (* check governor *)
-
-        (* check intel *)
-        (* disable boost *)
-
-
+        (* check cpu governor *)
+        printf "Set CPU scaling governor on isolated cores: \n";
+        Cores.iter (fun cpu ->
+                let f = sprintf "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor" cpu in
+                if (Sys.file_exists f) then begin
+                        let orig = read_file f in
+                        write_file f "performance";
+                        printf " - CPU core %d -> %s (was %s)\n" cpu (read_file f) orig
+                end
+        ) isol_cores;
